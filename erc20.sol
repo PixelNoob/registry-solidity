fa2164b359be43017e19c47d10518e9214a6e535
J. Chitty <jesuschitty7@hotmail.com>
2023-04-27 12:25:11 +0200

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ChittyCoin is ERC20, ERC20Burnable {
+contract RegistryCoin is ERC20, ERC20Burnable {
     constructor() ERC20("Registry", "REG") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
